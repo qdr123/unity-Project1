1d6a7dfc6426d2fa56b560e8986aab14e6a162a1
qdr123 <tmxjdydrb12@gmail.com>
2020-06-08 13:57:38 +0900

    알파버전 모델교체
    -백그라운드  스크롤링
    -캐릭터 모델 교체 (플레이어 에너미 보스)

---
+++ Unity Project/Assets/PYG/Scenes/Background.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    Material ma;
+    private float y = 0.5f;
+    float Offset;
+    Vector2 v2;
+    // Start is called before the first frame update
+    void Start()
+    {
+       ma = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //  ma.SetTextureOffset("_MainTex",v2(0, Offset));
+        BackgroundScroll();
+
+    }
+    private void BackgroundScroll()
+    {
+        Offset += Time.deltaTime * y;
+        Vector2 offset= ma.mainTextureOffset;
+        offset.Set(0, Offset);
+        ma.mainTextureOffset = offset;
+    }
+}
